62b52d770d2c4a452444b4deefbd73b2ca55927f
Jay Bazuzi <jay@bazuzi.com>
2024-01-23 14:54:54 -0800

    . r DRY: build_and_test in TypeScript

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -1,3 +1,1 @@
-@call %~dp0denow test --allow-all --parallel || exit /b
-@call %~dp0denow lint || exit /b
-@call %~dp0denow fmt --check || exit /b
+@call denow.cmd run --allow-all _build_and_test.ts
